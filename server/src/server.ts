b71745335ed76c1ff754c960cfe37bb67d1b0ab9
Abdul Hannan Khan <155804007+Hannank485@users.noreply.github.com>
2026-02-13 11:28:14 +0300

    chore:Set default PORT to 3000 if not defined

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ if (!process.env.ACCESS_SECRET_TOKEN) {
 }
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
